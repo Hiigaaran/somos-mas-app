0d6980b181384d49efe1dfc5b455912491af7dc2
ccastillocc <ccastillom@outlook.cl>
2022-06-20 17:50:46 -0400

    OK TA25 y 26

--- app/src/main/java/com/example/somosmasapp/views/LogInViewModel.kt
+++ app/src/main/java/com/example/somosmasapp/views/LogInViewModel.kt
@@ -5,9 +5,20 @@ import androidx.core.util.PatternsCompat
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.util.regex.Pattern
+import com.example.somosmasapp.data.OngApiRepository
+import com.example.somosmasapp.data.ResponseListener
+import com.example.somosmasapp.data.dto.*
+import com.example.somosmasapp.data.dto.Login
 
-class LogInViewModel:ViewModel(){
+class LoginViewModel(private val repository: OngApiRepository): ViewModel() {
     val blockButton = MutableLiveData<Boolean>(false)
+    val success = MutableLiveData<Boolean>(null)
+    val error = MutableLiveData<String>(null)
+    val errors = MutableLiveData<Errors?>(null)
+    val message = MutableLiveData<String>(null)
+    val user = MutableLiveData<User>(null)
+    val token = MutableLiveData<String>(null)
+
     private fun checkPassword(pass:String):Boolean{
         val passwordRegex =  Pattern.compile(
             "^\\w\\w\\w\\w+$"
@@ -28,5 +39,29 @@ class LogInViewModel:ViewModel(){
         val resultado = checkEmail(email) && checkPassword(pass)
         blockButton.value = !resultado
 
+    }
+
+    fun doLogin(body: Login){
+        success.value = null
+        error.value = null
+        message.value = null
+        user.value = null
+        token.value = null
+
+        repository.doLogin(body, object : ResponseListener<UserRegister>{
+            override fun onResponse(response: RepositoryResponse<UserRegister>) {
+                if (response.success) {
+                    user.value = response.data.user
+                    token.value = response.data.token
+                }
+                error.value = response.error
+                success.value = response.success
+                message.value = response.message
+            }
+
+            override fun onError(repositoryError: RepositoryError) {
+                errors.value = repositoryError.errors
+            }
+        })
     }
 }
