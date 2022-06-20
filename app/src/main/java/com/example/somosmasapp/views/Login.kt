d43ff043665411d239b4516da61bc37d4e79d064
nicole.quezada.godoy <n.nataly@hotmail.com>
2022-06-20 14:35:16 -0400

    Revert "Merge pull request #11 from Hiigaaran/TA02-24"
    This reverts commit 312278a3bf0d10bdef2441484eb804296edd16ec, reversing
    changes made to 328f1e0f41698194f5fd3f3e9d32d24b1371a2ea.

--- app/src/main/java/com/example/somosmasapp/views/Login.kt
+++ app/src/main/java/com/example/somosmasapp/views/Login.kt
@@ -2,38 +2,24 @@ package com.example.somosmasapp.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.viewModels
-import androidx.core.util.PatternsCompat
-import androidx.core.widget.addTextChangedListener
-import androidx.lifecycle.ViewModel
+import com.example.somosmasapp.R
 import com.example.somosmasapp.databinding.WindowLoginBinding
-import java.util.regex.Pattern
+import android.content.Intent
 
 
 class Login : AppCompatActivity() {
-    private val viewModel:LogInViewModel by viewModels()
+
     private lateinit var binding : WindowLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.window_login)
         binding = WindowLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonEntrarlogin.isEnabled = false
-
-
-        binding.editEmaillogin.addTextChangedListener {
-            viewModel.check(binding.editEmaillogin.text.toString(),binding.editPasswordlogin.text.toString())
+        binding.textViewOlvidarlogin.setOnClickListener{
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
         }
-        binding.editPasswordlogin.addTextChangedListener{
-            viewModel.check(binding.editEmaillogin.text.toString(),binding.editPasswordlogin.text.toString())
-        }
-
-        viewModel.blockButton.observe(this){ value->
-            binding.buttonEntrarlogin.isEnabled = !value
-        }
-
     }
-
 }
