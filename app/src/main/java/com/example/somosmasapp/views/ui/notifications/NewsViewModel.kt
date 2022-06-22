6d648544810009ffd58c68c37c2cd2fbdef9dc03
Hiigaaran <simo.moralesg@gmail.com>
2022-06-22 17:47:02 -0400

    ta02-56 se implementa viewmodel de fragment preliminar

--- app/src/main/java/com/example/somosmasapp/views/ui/notifications/NewsViewModel.kt
+++ app/src/main/java/com/example/somosmasapp/views/ui/notifications/NewsViewModel.kt
@@ -1,13 +1,37 @@
 package com.example.somosmasapp.views.ui.notifications
 
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.somosmasapp.data.OngApiRepository
+import com.example.somosmasapp.data.ResponseListener
+import com.example.somosmasapp.data.dto.News
+import com.example.somosmasapp.data.dto.RepositoryError
+import com.example.somosmasapp.data.dto.RepositoryResponse
 
-class NewsViewModel : ViewModel() {
+class NewsViewModel(private val repository: OngApiRepository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    val success = MutableLiveData<Boolean>(false)
+    val data = MutableLiveData<List<News>>(null)
+
+    fun getNews() {
+        success.value = false
+        data.value = null
+
+        repository.getNews(object: ResponseListener<List<News>> {
+            override fun onResponse(response: RepositoryResponse<List<News>>) {
+                if(null != response && response.success) {
+                    success.value = response.success
+                    data.value = response.data
+                }
+            }
+
+            override fun onError(repositoryError: RepositoryError) {
+                Toast.makeText(null, "Error getting imageSlider data", Toast.LENGTH_LONG)
+            }
+
+        })
     }
-    val text: LiveData<String> = _text
+
 }
