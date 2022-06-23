53a47799ee82a862d0a3f9de8a1c9dc1c0bc2766
Johanna Goenaga <johannagoenagac@gmail.com>
2022-06-23 11:40:42 -0400

    Add layout home

--- app/src/main/java/com/example/somosmasapp/views/HomeActivity.kt
+++ app/src/main/java/com/example/somosmasapp/views/HomeActivity.kt
@@ -23,7 +23,6 @@ class HomeActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-
         val navController = Navigation.findNavController(this,R.id.nav_host_fragment_activity_home)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
